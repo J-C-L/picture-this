88e8ee51aff8cf4b3b4be23e378a31ced66b4fdf
Janice Lichtman <jalichtman@gmail.com>
2017-07-13 15:27:51 -0700

    broke and fixed things

--- src/components/UploadScreen.js
+++ src/components/UploadScreen.js
@@ -10,37 +10,34 @@ class UploadScreen extends Component {
       fileToBeSent:[],
     }
   }
-  onDrop(acceptedFiles, rejectedFiles) {
-
+  onDrop(acceptedFile, rejectedFile) {
     var fileToBeSent=this.state.fileToBeSent;
-    fileToBeSent.push(acceptedFiles);
+    fileToBeSent.push(acceptedFile);
     this.setState({fileToBeSent});
 
-    var filePreview=[];
-    for(var i in fileToBeSent){
-      filePreview.push(<div>
-        {fileToBeSent[i][0].name}
 
-        </div>
-      )
-    }
-      this.setState({fileToBeSent,filePreview});
-      console.log(this.state.fileToBeSent[0]);
+    console.log(this.state.fileToBeSent)
+    var filePreview=(<div>
+      {fileToBeSent[0][0].name}
+      </div>
+    )
+    this.setState({filePreview: filePreview})
+    console.log(this.state.fileToBeSent[0][0].name);
   }
 
-    render() {
-      return (
-        <div className="Upload">
-        <Dropzone onDrop={(files) => this.onDrop(files)}>
-        <div>Drag your data file here, or click to select file to upload.</div>
-        </Dropzone>
-        <div>
-        File to be uploaded is:
-        {this.state.filePreview}
-        </div>
-        </div>
-      )
-    }
+  render() {
+    return (
+      <div className="Upload">
+      <Dropzone onDrop={(files) => this.onDrop(files)}>
+      <div>Drag your data file here, or click to select file to upload.</div>
+      </Dropzone>
+      <div>
+      File to be uploaded is:
+      {this.state.filePreview}
+      </div>
+      </div>
+    )
   }
+}
 
-  export default UploadScreen;
+export default UploadScreen;
