c5378c18a1845db4bc60e28719bfe8ce2adcc0ca
Janice Lichtman <jalichtman@gmail.com>
2017-07-20 15:46:37 -0700

    catches error if line graph does not support chosen data type

---
+++ src/components/GraphD3.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import {groupBy, reduce} from 'lodash';
+//_ is a common symbol for lodash, so could use
+// import _ from 'lodash';
+import d3 from 'd3';
+
+
+class GraphD3 extends Component {
+
+  componentDidMount() {
+    this.updateChart();
+  }
+  componentDidUpdate() {
+    this.updateChart();
+  }
+  updateChart() {
+    var data = [4, 8, 15, 16, 23, 42];
+
+    d3.select("#chart")
+      .selectAll("div")
+        .data(data)
+      .enter().append("div")
+        .attr("class", "bar")
+        .style("width", function(d) { return d * 10 + "px"; })
+        .text(function(d) { return d; });
+  }
+
+
+  render() {
+      return (
+        <div>
+          <div id="chart"> D3 Chart Will Go Here</div>
+        </div>
+      )
+    }
+  }
+
+  export default GraphD3;
