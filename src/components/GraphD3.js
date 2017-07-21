160d8e875d943168729f145686ae84576ba0c634
Janice Lichtman <jalichtman@gmail.com>
2017-07-21 10:01:13 -0700

    has dummy d3 component

--- src/components/GraphD3.js
+++ src/components/GraphD3.js
@@ -14,15 +14,16 @@ class GraphD3 extends Component {
     this.updateChart();
   }
   updateChart() {
-    var data = [4, 8, 15, 16, 23, 42];
-
-    d3.select("#chart")
-      .selectAll("div")
-        .data(data)
-      .enter().append("div")
-        .attr("class", "bar")
-        .style("width", function(d) { return d * 10 + "px"; })
-        .text(function(d) { return d; });
+    // var data = [4, 8, 15, 16, 23, 42];
+    //
+    // d3.select("#chart")
+    //   .selectAll("div")
+    //     .data(data)
+    //   .enter().append("div")
+    //     .attr("class", "bar")
+    //     .style("width", function(d) { return d * 10 + "px"; })
+    //     .text(function(d) { return d; });
+    return
   }
 
 
