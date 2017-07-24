8b57b19392cee4a7b98e4bc205bdeb14b822f6ee
Janice Lichtman <jalichtman@gmail.com>
2017-07-24 14:42:59 -0700

    sorted data by frequency so bar chart shows data in descending order

--- src/components/GraphBar.js
+++ src/components/GraphBar.js
@@ -25,114 +25,117 @@ class GraphBar extends Component {
   }
 
   updateChart() {
+    // creates an object of objects, where each inner object has a key of the category name, and a value that is an array of each csv data row as a js object
     const groupedData = groupBy(this.props.dataToGraph, this.state.category);
 
-
+    // creates array of arrays, where each inner array has 2 values, the category name, and the frequency of occurances of that category.
     const categoryLengths = reduce(groupedData, (result, value, key) =>
     {
       result.push([key, value.length]);
       return result;
     }, []);
 
-    console.log(categoryLengths);
+    // Sorts the categoryLengths array by descending frequency of occurrances
+    const sortedDescCategoryLengths=categoryLengths.sort(function(a,b){
+      return b[1] - a[1];
+    });
+
 
     const categories =['x'];
     for (var i=0; i<categoryLengths.length; i++){
-      categories.push(categoryLengths[i][0]);
+      categories.push(sortedDescCategoryLengths[i][0]);
     }
 
     const data=[this.state.category];
     for (var i=0; i<categoryLengths.length; i++){
-      data.push(categoryLengths[i][1]);
+      data.push(sortedDescCategoryLengths[i][1]);
     }
 
-    console.log(categoryLengths);
-    console.log(categories);
-    console.log(data);
+
 
     // c3.generate({
-    //   bindto: '#chart',
-    //   data: {
-    //     x: 'x',
-    //     columns: [
-    //       categories,
-    //       data,
-    //     ],
-    //     type: this.props.chartType.toLowerCase(),
-    //     labels: true,
-    //   },
-    //
-    //   axis: {
-    //     x: {
-    //       type: 'category' // this needed to load string x value
+    //     bindto: '#chart',
+    //     data: {
+    //       x: 'x',
+    //       columns: [
+    //         categories,
+    //         data,
+    //       ],
+    //       type: this.props.chartType.toLowerCase(),
+    //       labels: true,
     //     },
-    //     y:{
-    //       label: {
-    //         text: 'Totals',
-    //         position: 'outer-middle'
+    //
+    //     axis: {
+    //       x: {
+    //         type: 'category' // this needed to load string x value
+    //       },
+    //       y:{
+    //         label: {
+    //           text: 'Totals',
+    //           position: 'outer-middle'
+    //         }
     //       }
-    //     }
-    //   },
-    //   // bar: {
-    //   //   width: {
-    //   //     ratio: 0.8 // this makes bar width 80% of length between ticks. Default is 0.6
-    //   //   }
-    //   // }
-    //   });
+    //     },
+    //     // bar: {
+    //     //   width: {
+    //     //     ratio: 0.8 // this makes bar width 80% of length between ticks. Default is 0.6
+    //     //   }
+    //     // }
+    //     });
 
 
-
+      
       c3.generate({
         bindto: '#chart',
         data: {
-
           x: 'x',
-            columns:
-            [['x', this.state.category]].concat(categoryLengths)  ,
+          columns:
+          [['x', this.state.category]].concat(sortedDescCategoryLengths),
           // columns: categoryLengths,
           type: this.props.chartType.toLowerCase(),
-          labels: true
+          labels: true,
         },
         axis: {
-            x: {
-              type: 'category' // this needed to load string x value
-            },
-            y:{
-              label: {
-                text: 'Totals',
-                position: 'outer-middle'
-              }
+          x: {
+            type: 'category' // this needed to load string x value
+          },
+          y:{
+            label: {
+              text: 'Totals',
+              position: 'outer-middle'
             }
           }
-      });
+        },
+        tooltip: {
+          show: false
+          }
+        });
 
+      }
 
+      render() {
 
-    }
-
-    render() {
-
-      if (!this.state.category){
-        return (
-          <DataCategoryDropdown
-            category={this.state.category} dataToGraph={this.props.dataToGraph} onCategorySelect={category => this.setState({category})} />
-        )
-      }else{
-        return (
-          <div>
-
+        if (!this.state.category){
+          return (
             <DataCategoryDropdown
               category={this.state.category} dataToGraph={this.props.dataToGraph} onCategorySelect={category => this.setState({category})} />
+          )
+        }else{
+          return (
+            <div>
 
-            <h2 className="chart-title">
-              File Being Graphed: {this.props.name} </h2>
-            <h4 className="chart-title"> Chart Type: {this.props.chartType} </h4>
-            <h4 className="chart-title"> Category shown: {this.state.category} </h4>
-            <div id="chart"></div>
-          </div>
-        )
+              <DataCategoryDropdown
+                category={this.state.category} dataToGraph={this.props.dataToGraph} onCategorySelect={category => this.setState({category})} />
+
+              <h2 className="chart-title">
+                File Being Graphed: {this.props.name} </h2>
+              <h4 className="chart-title"> Chart Type: {this.props.chartType} </h4>
+              <h4 className="chart-title"> Category shown: {this.state.category} </h4>
+              <div id="chart"></div>
+            </div>
+          )
+        }
       }
     }
-  }
 
-  export default GraphBar;
+    export default GraphBar;
